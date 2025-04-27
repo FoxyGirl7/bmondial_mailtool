4bd88bf42ac0c4fb5b2c3dfe7cf1c6f267053d70
FoxyGirl7 <samya.boutaba23@ump.ac.ma>
2025-04-27 19:59:56 +0100

    Setup dashboard route and view with React app

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <title>Dashboard</title>
   @viteReactRefresh
-  @vite('resources/js/app.js')
+  @vite('resources/js/app.jsx')
 </head>
 <body>
   <div id="app"></div> <!-- React will render inside this div -->
